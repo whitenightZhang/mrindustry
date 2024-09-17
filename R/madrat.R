3d6c719f623f5ed56cca109b845e83d0a62a72ac
Falk Benke <falk.benke@pik-potsdam.de>
2024-09-17 17:54:20 +0200

    mark as madrat package

---
+++ R/madrat.R
@@ -0,0 +1,14 @@
+
+.onAttach <- function(libname, pkgname) {
+  madrat::madratAttach(pkgname)
+}
+
+.onDetach <- function(libpath) {
+  madrat::madratDetach(libpath)
+}
+
+# redirect standard messaging functions to vcat
+cat     <- function(...) vcat(1, ...)
+message <- function(...) vcat(1, ...)
+warning <- function(...) vcat(0, ...)
+stop    <- function(...) vcat(-1, ...)
