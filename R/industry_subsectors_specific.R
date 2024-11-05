b12191bfbdde91eb4ed38dcf3edda996c7b9f460
Michaja Pehl <pehl@pik-potsdam.de>
2024-11-05 09:18:17 +0100

    correct filename for spcific FE data

--- R/industry_subsectors_specific.R
+++ R/industry_subsectors_specific.R
@@ -64,7 +64,7 @@ readindustry_subsectors_specific <- function(subtype = NULL) {
   # subtype switchboard ----
   switchboard <- list(
     'FE' = function() {
-      read_csv(file = file.path(path, 'specific_FE.SSP3.csv'),
+      read_csv(file = file.path(path, 'specific_FE.csv'),
                col_types = 'cccd',
                comment = '#',
                progress = FALSE) %>%
