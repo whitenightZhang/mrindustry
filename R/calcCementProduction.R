ce49e0482d746edf7afb11c2fe92fb6c4ed5d652
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-02-27 12:40:26 +0100

    correct comments, run lucode2::buildLibrary(), add author

--- R/calcCementProduction.R
+++ R/calcCementProduction.R
@@ -1,31 +1,18 @@
 #' Calculates global cement production as from Andrew's 2019 paper.
 #' @author Bennet Weiss
-#' @param x Magpie object
+#' @param x magclass object
 calcCementProduction <- function(x) {
-  x <- readSource("Andrew2019", convert=TRUE)
+  x <- readSource("Andrew2019", convert = TRUE)
   x[is.na(x)] <- 0 # TODO implement smarter way to fill NA values
-  x = x * 1e3 # convert to tonnes
+  x <- x * 1e3 # convert to tonnes
   unit <- "tonnes (t)"
-  description <- paste("Annual Cement Production as from",
-                      "Andrew, R.M., 2019. Global CO2 emissions from cement production, 1928–2018.",
-                      "Earth System Science Data 11, 1675–1710. https://doi.org/10.5194/essd-11-1675-2019.",
-                      "Data reported on https://zenodo.org/records/11207133.",
-                      "Accessed: 24.02.2025.")
+  description <- paste(
+    "Annual Cement Production as from",
+    "Andrew, R.M., 2019. Global CO2 emissions from cement production, 1928-2018.",
+    "Earth System Science Data 11, 1675-1710. https://doi.org/10.5194/essd-11-1675-2019.",
+    "Data reported on https://zenodo.org/records/11207133.",
+    "Accessed: 24.02.2025."
+  )
   output <- list(x = x, weight = NULL, unit = unit, description = description)
   return(output)
-}
-
-#' Plots and saves cement production for each country separately.
-#' @author Bennet Weiss
-plotCementProduction <- function(x) {
-  df <- as.data.frame(x)
-  regions <- unique(df$Region)
-  for(region in regions) {
-    print(region)
-    dfr <- filter(df, df$Region == region)
-    png(filename = paste("figures/", region, "cement_production.png", sep = ""))
-    plot(dfr$Year, dfr$Value, type = "p", col = "blue", pch = 16,
-         xlab = "Time", ylab = "Cement Production (t)", main = region)
-    dev.off()
-  }
 }
--- R/convertAndrew2019.R
+++ R/convertAndrew2019.R
@@ -2,19 +2,20 @@
 #' @author Bennet Weiss
 #' @param x Magpie object
 convertAndrew2019 <- function(x) {
-    no_remove_warning <- c(
-      "VDR", "East & West Pakistan", "Federation of Malaya-Singapore", "YMD", "FORMER YEMEN",
-      "French Indo-China", "Japan (Excluding The Ruyuku Islands)", "ANT", "Netherland Antilles and Aruba",
-      "Peninsular Malaysia", "Republic of South Vietnam", "Rhodesia-Nyasaland", "Rwanda-Urundi", "Sabah",
-      "Sarawak", "SUDAN", "Tanganyika", "United Korea", "Zanzibar", # countries with NA entries
-      "DDR", "DEW", # only one value in 1990
-      "KNA___215", "KNA___216", "PCZ", "French Equatorial Africa", "Kuwaiti Oil Fires", "Leeward Islands", "Pacific Islands (Palau)", # zero anyways
-      "French West Africa", "KSV", "Ryukyu Islands" # unsure
-    )
-    magclass::getItems(x, dim=1)[which(getItems(x, dim=1) == "YEMEN")] <- "YEM"
-    magclass::getItems(x, dim=1)[which(getItems(x, dim=1) == "REPUBLIC OF SUDAN")] <- "SDN"
-    x <- madrat::toolISOhistorical(x, overwrite = FALSE) # probably not needed
-    x <- madrat::toolCountryFill(x, fill=NA, verbosity=1, no_remove_warning=no_remove_warning)
-    # Puerto Rico is missing in dataset. Probably no problem.
-    return(x)
+  no_remove_warning <- c(
+    "VDR", "East & West Pakistan", "Federation of Malaya-Singapore", "YMD", "FORMER YEMEN",
+    "French Indo-China", "Japan (Excluding The Ruyuku Islands)", "ANT", "Netherland Antilles and Aruba",
+    "Peninsular Malaysia", "Republic of South Vietnam", "Rhodesia-Nyasaland", "Rwanda-Urundi", "Sabah",
+    "Sarawak", "SUDAN", "Tanganyika", "United Korea", "Zanzibar", # countries with NA entries
+    "DDR", "DEW", # only one value in 1990
+    "KNA___215", "KNA___216", "PCZ", "French Equatorial Africa", "Kuwaiti Oil Fires",
+    "Leeward Islands", "Pacific Islands (Palau)", # zero anyways
+    "French West Africa", "KSV", "Ryukyu Islands" # unsure
+  )
+  magclass::getItems(x, dim = 1)[which(getItems(x, dim = 1) == "YEMEN")] <- "YEM"
+  magclass::getItems(x, dim = 1)[which(getItems(x, dim = 1) == "REPUBLIC OF SUDAN")] <- "SDN"
+  x <- madrat::toolISOhistorical(x, overwrite = FALSE) # probably not needed
+  x <- madrat::toolCountryFill(x, fill = NA, verbosity = 2, no_remove_warning = no_remove_warning)
+  # Puerto Rico is missing in dataset. Probably no problem.
+  return(x)
 }
