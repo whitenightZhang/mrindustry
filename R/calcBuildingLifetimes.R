8da198382ef547e07709032882b96a1c59e57430
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-04-22 17:30:50 +0200

    improve convert function with regionmapping

--- R/calcBuildingLifetimes.R
+++ R/calcBuildingLifetimes.R
@@ -1,9 +1,14 @@
 #' Calculates the lifetimes of residential and non-residential buildings, as well as of civil engeneering.
 #' @author Bennet Weiss
 #' @param x magclass object
-calcBuildingLifetimes <- function(x, subtype) {
-    # TODO check if i should do subtypes as additional variable of function
+calcBuildingLifetimes <- function(x) {
     res <- readSource("PostedLifetimes", convert = TRUE, subtype = "Res")
+    nonres <- readSource("PostedLifetimes", convert = TRUE, subtype = "NonRes")
+    civ <- readSource("PostedLifetimes", convert = TRUE, subtype = "Civ")
 
-    # add missing regions/countries, calculate averages, etc...
+    # examples: 
+    # 1. set nonres value from china to its res value
+    # 2. set OAS to average of China, India and Japan
+
+    # return the combination of res, nonres, civ.
 }
--- R/convertPostedLifetimes.R
+++ R/convertPostedLifetimes.R
@@ -1,7 +1,30 @@
 #' convert data from PostedLifetimes
 #' @author Bennet Weiss
 #' @param x Magpie object
+#' @param subtype End-use type of construction. Can be either "Res", "NonRes" or "Civ".
 convertPostedLifetimes <- function(x) {
-    # get country situation right
-    # find out how region level data works in madrat
+    # convert country name to iso3 if applicable, other region names stay untouched
+    regions <- magclass::getItems(x, dim = 1)
+    isocodes <- madrat::toolCountry2isocode(regions)
+    magclass::getItems(x, dim = 1)[!is.na(isocodes)] <- isocodes[!is.na(isocodes)]
+    x_out <- madrat::toolCountryFill(x, fill = NA, verbosity = 2)
+
+    # fill values by region
+    # regionmapping also found in rd3mod/inputdata/mappings/regional
+    regionmapping <- toolGetMapping("regionmapping_21_EU11.csv", where = "mrindustry", type = "regional")
+    
+    # fill all EUR regions with Europe value
+    eur_regions <- unique(regionmapping$RegionCode[regionmapping$missingH12 == "EUR"])
+    eur_mask <- magclass::getItems(x_out, dim = 1) %in% regionmapping$CountryCode[regionmapping$RegionCode %in% eur_regions]
+    eur_countries <- magclass::getItems(x_out, dim = 1)[eur_mask]
+    x_out[eur_countries,,] <- x["Europe",,]
+
+    # fill all LAM regions with Rest of South America value
+    lam_mask <- magclass::getItems(x_out, dim = 1) %in% regionmapping$CountryCode[regionmapping$RegionCode == "LAM"]
+    lam_countries <- magclass::getItems(x_out, dim = 1)[lam_mask]
+    x_out[lam_countries,,] <- x["Rest of South America",,]
+    
+    # TODO: set China data to be constant in beginning and end of time period
+
+    return(x_out)
 }
