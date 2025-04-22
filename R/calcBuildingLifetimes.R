4bc36de075bb0bb7914eaa9ee8d8a52549c13a2c
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-04-22 18:02:32 +0200

    minor changes

--- R/calcBuildingLifetimes.R
+++ R/calcBuildingLifetimes.R
@@ -5,10 +5,12 @@ calcBuildingLifetimes <- function(x) {
     res <- readSource("PostedLifetimes", convert = TRUE, subtype = "Res")
     nonres <- readSource("PostedLifetimes", convert = TRUE, subtype = "NonRes")
     civ <- readSource("PostedLifetimes", convert = TRUE, subtype = "Civ")
+    combined <- mbind(res, nonres, civ)
 
     # examples: 
     # 1. set nonres value from china to its res value
     # 2. set OAS to average of China, India and Japan
+
 
     # return the combination of res, nonres, civ.
 }
--- R/convertPostedLifetimes.R
+++ R/convertPostedLifetimes.R
@@ -2,7 +2,7 @@
 #' @author Bennet Weiss
 #' @param x Magpie object
 #' @param subtype End-use type of construction. Can be either "Res", "NonRes" or "Civ".
-convertPostedLifetimes <- function(x) {
+convertPostedLifetimes <- function(x, subtype) {
     # convert country name to iso3 if applicable, other region names stay untouched
     regions <- magclass::getItems(x, dim = 1)
     isocodes <- madrat::toolCountry2isocode(regions)
@@ -10,8 +10,8 @@ convertPostedLifetimes <- function(x) {
     x_out <- madrat::toolCountryFill(x, fill = NA, verbosity = 2)
 
     # fill values by region
-    # regionmapping also found in rd3mod/inputdata/mappings/regional
-    regionmapping <- toolGetMapping("regionmapping_21_EU11.csv", where = "mrindustry", type = "regional")
+    # regionmapping also found in rd3mod/inputdata/mappings/regional TODO: rausehmen
+    regionmapping <- toolGetMapping("regionmapping_21_EU11.csv", where = "mappingfolder", type = "regional")
     
     # fill all EUR regions with Europe value
     eur_regions <- unique(regionmapping$RegionCode[regionmapping$missingH12 == "EUR"])
@@ -25,6 +25,9 @@ convertPostedLifetimes <- function(x) {
     x_out[lam_countries,,] <- x["Rest of South America",,]
     
     # TODO: set China data to be constant in beginning and end of time period
+    # x_out["CHN",,][which(magclass:getItems(x_out, dim=2))]
+    # lam_countries
 
+    # OAS = (CHA + IND + JPN) / 3
     return(x_out)
 }
