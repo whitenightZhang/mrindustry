9c07813ec19958e63d8e83c94caf915eb9294555
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-04-30 16:07:43 +0200

    style + linter

--- R/convertPostedBuiltLifespan.R
+++ R/convertPostedBuiltLifespan.R
@@ -2,75 +2,76 @@
 #' @author Bennet Weiss
 #' @param x Magpie object
 convertPostedBuiltLifespan <- function(x) {
-    # convert country name to iso3 if applicable, other region names stay untouched
-    regions <- magclass::getItems(x, dim = 1)
-    isocodes <- madrat::toolCountry2isocode(regions)
-    magclass::getItems(x, dim = 1)[!is.na(isocodes)] <- isocodes[!is.na(isocodes)]
-    x_out <- madrat::toolCountryFill(x, fill = NA, verbosity = 2)
+  # convert country name to iso3 if applicable, other region names stay untouched
+  regions <- magclass::getItems(x, dim = 1)
+  no_remove_warning <- c("Africa", "CIS", "Europe", "Rest of South America")
+  isocodes <- madrat::toolCountry2isocode(regions, ignoreCountries = no_remove_warning)
+  magclass::getItems(x, dim = 1)[!is.na(isocodes)] <- isocodes[!is.na(isocodes)]
+  x_out <- madrat::toolCountryFill(x, fill = NA, verbosity = 2, no_remove_warning = no_remove_warning)
 
-    # fill values by region
-    # regionmapping also found in rd3mod/inputdata/mappings/regional TODO: rausehmen
-    regionmapping <- toolGetMapping("regionmapping_21_EU11.csv", where = "mappingfolder", type = "regional")
-    
-    # EUR
-    # fill all countries with European average
-    eur_regions <- unique(regionmapping$RegionCode[regionmapping$missingH12 == "EUR"])
-    eur_countries <- getRegionCountries(x_out, eur_regions, regionmapping)
-    x_out[eur_countries,,] <- x["Europe",,]
+  # fill values by region
+  # regionmapping also found in rd3mod/inputdata/mappings/regional TODO: rausehmen
+  regionmapping <- toolGetMapping("regionmapping_21_EU11.csv", where = "mappingfolder", type = "regional")
 
-    # NEU
-    # EUR values
-    neu_regions <- unique(regionmapping$RegionCode[regionmapping$missingH12 == "NEU"])
-    neu_countries <- getRegionCountries(x_out, neu_regions, regionmapping)
-    x_out[neu_countries,,] <- x["Europe",,]
+  # EUR
+  # fill all countries with European average
+  eur_regions <- unique(regionmapping$RegionCode[regionmapping$missingH12 == "EUR"])
+  eur_countries <- getRegionCountries(x_out, eur_regions, regionmapping)
+  x_out[eur_countries, , ] <- x["Europe", , ]
 
-    # LAM
-    # "Rest of South America" Residential values
-    lam_countries <- getRegionCountries(x_out, "LAM", regionmapping)
-    x_out[lam_countries,,] <- x["Rest of South America",,"Res"]
-    
-    # CHN 
-    # fill all years before 1950 with 1950 value and all years after 2015 with 2015 value
-    # fill non-residential with residential values (following Cao 2019)
-    first_non_na <- x_out["CHN", 1950 ,"Res"]
-    last_non_na <- x_out["CHN", 2015 ,"Res"]
-    x_out["CHN",,"Res"][magclass::getYears(x_out, as.integer=TRUE) < 1950] <- first_non_na
-    x_out["CHN",,"Res"][magclass::getYears(x_out, as.integer=TRUE) > 2015] <- last_non_na
-    x_out["CHN",,"NonRes"] <- x_out["CHN",,"Res"]
+  # NEU
+  # EUR values
+  neu_regions <- unique(regionmapping$RegionCode[regionmapping$missingH12 == "NEU"])
+  neu_countries <- getRegionCountries(x_out, neu_regions, regionmapping)
+  x_out[neu_countries, , ] <- x["Europe", , ]
 
-    # CHA
-    # copy CHN values
-    cha_countries <- getRegionCountries(x_out, "CHA", regionmapping)
-    x_out[cha_countries,,] <- x_out["CHN",,]
+  # LAM
+  # "Rest of South America" Residential values
+  lam_countries <- getRegionCountries(x_out, "LAM", regionmapping)
+  x_out[lam_countries, , ] <- x["Rest of South America", , "Res"]
 
-    # OAS
-    # average of China, India and Japan
-    oas_countries <- getRegionCountries(x_out, "OAS", regionmapping)
-    x_out[oas_countries,,] <- (x_out["CHN",,] + x_out["IND",,] + x_out["JPN",,]) / 3
+  # CHN
+  # fill all years before 1950 with 1950 value and all years after 2015 with 2015 value
+  # fill non-residential with residential values (following Cao 2019)
+  first_non_na <- x_out["CHN", 1950, "Res"]
+  last_non_na <- x_out["CHN", 2015, "Res"]
+  x_out["CHN", , "Res"][magclass::getYears(x_out, as.integer = TRUE) < 1950] <- first_non_na
+  x_out["CHN", , "Res"][magclass::getYears(x_out, as.integer = TRUE) > 2015] <- last_non_na
+  x_out["CHN", , "NonRes"] <- x_out["CHN", , "Res"]
 
-    # CAZ
-    # average of EU and USA
-    caz_countries <- getRegionCountries(x_out, "CAZ", regionmapping)
-    x_out[caz_countries,,] <- (x["Europe",,] + x_out["USA",,]) / 2
+  # CHA
+  # copy CHN values
+  cha_countries <- getRegionCountries(x_out, "CHA", regionmapping)
+  x_out[cha_countries, , ] <- x_out["CHN", , ]
 
-    # MEA
-    # values from Iran 
-    mea_countries <- getRegionCountries(x_out, "MEA", regionmapping)
-    x_out[mea_countries,,] <- x_out["IRN",,]
+  # OAS
+  # average of China, India and Japan
+  oas_countries <- getRegionCountries(x_out, "OAS", regionmapping)
+  x_out[oas_countries, , ] <- (x_out["CHN", , ] + x_out["IND", , ] + x_out["JPN", , ]) / 3
 
-    # SSA
-    # values from Africa
-    ssa_countries <- getRegionCountries(x_out, "SSA", regionmapping)
-    x_out[ssa_countries,,] <- x["Africa",,]
+  # CAZ
+  # average of EU and USA
+  caz_countries <- getRegionCountries(x_out, "CAZ", regionmapping)
+  x_out[caz_countries, , ] <- (x["Europe", , ] + x_out["USA", , ]) / 2
 
-    # REF
-    # values from CIS
-    ref_countries <- getRegionCountries(x_out, "REF", regionmapping)
-    x_out[ref_countries,,] <- x["CIS",,]
+  # MEA
+  # values from Iran
+  mea_countries <- getRegionCountries(x_out, "MEA", regionmapping)
+  x_out[mea_countries, , ] <- x_out["IRN", , ]
 
-    # IND, JPN, USA are fine by default
+  # SSA
+  # values from Africa
+  ssa_countries <- getRegionCountries(x_out, "SSA", regionmapping)
+  x_out[ssa_countries, , ] <- x["Africa", , ]
 
-    return(x_out)
+  # REF
+  # values from CIS
+  ref_countries <- getRegionCountries(x_out, "REF", regionmapping)
+  x_out[ref_countries, , ] <- x["CIS", , ]
+
+  # IND, JPN, USA are fine by default
+
+  return(x_out)
 }
 
 #' @author Bennet Weiss
@@ -78,8 +79,8 @@ convertPostedBuiltLifespan <- function(x) {
 #' @param region string or vector of strings to identify the region
 #' @param regionmapping mapping of regions to countries
 getRegionMask <- function(x, region, regionmapping) {
-    mask <- magclass::getItems(x, dim = 1) %in% regionmapping$CountryCode[regionmapping$RegionCode %in% region]
-    return(mask)
+  mask <- magclass::getItems(x, dim = 1) %in% regionmapping$CountryCode[regionmapping$RegionCode %in% region]
+  return(mask)
 }
 
 #' @author Bennet Weiss
@@ -87,7 +88,7 @@ getRegionMask <- function(x, region, regionmapping) {
 #' @param region string or vector of strings to identify the region
 #' @param regionmapping mapping of regions to countries
 getRegionCountries <- function(x, region, regionmapping) {
-    regionmask <- getRegionMask(x, region, regionmapping)
-    countries <- magclass::getItems(x, dim = 1)[regionmask]
-    return(countries)
+  regionmask <- getRegionMask(x, region, regionmapping)
+  countries <- magclass::getItems(x, dim = 1)[regionmask]
+  return(countries)
 }
--- R/readPostedBuiltLifespan.R
+++ R/readPostedBuiltLifespan.R
@@ -3,30 +3,32 @@
 #' Data sources can be found in Posted
 #' @author Bennet Weiss
 readPostedBuiltLifespan <- function() {
-    path <- file.path("v1", "buildings_and_infrastructure_lifetime.csv")
-    data <- suppressMessages(readr::read_csv(path))
+  path <- file.path("v1", "buildings_and_infrastructure_lifetime.csv")
+  data <- suppressMessages(readr::read_csv(path))
 
-    # remove unnecessary colums
-    cleaned_data <- data %>%
-        select(-period, -unit, -variable)
-    
-    # expand time range into individual years
-    cleaned_data <- separate(cleaned_data, time_range, into = c("start_year", "end_year"), sep = "-")
-    # create a sequence of years for each row
-    cleaned_data$time <- mapply(function(start, end) seq(start, end), 
-                            cleaned_data$start_year, 
-                            cleaned_data$end_year, 
-                            SIMPLIFY = FALSE)
-    # expand this year sequence into separate rows
-    expanded_data <- unnest(cleaned_data, cols = c(time))
+  # remove unnecessary colums
+  cleaned_data <- data %>%
+    select(-all_of(c("period", "unit", "variable")))
 
-    # remove unnecessary columns
-    expanded_data$time_range <- NULL
-    expanded_data$start_year <- NULL
-    expanded_data$end_year <- NULL
+  # expand time range into individual years
+  cleaned_data <- cleaned_data %>%
+    separate(all_of("time_range"), into = c("start_year", "end_year"), sep = "-")
+  # create a sequence of years for each row
+  cleaned_data$time <- Map(
+    seq,
+    cleaned_data$start_year,
+    cleaned_data$end_year
+  )
+  # expand this year sequence into separate rows
+  expanded_data <- unnest(cleaned_data, cols = c(time))
 
-    ordered_data <- expanded_data[, c("region", "time", "end_use", "value")]
+  # remove unnecessary columns
+  expanded_data$time_range <- NULL
+  expanded_data$start_year <- NULL
+  expanded_data$end_year <- NULL
 
-    x <- magclass::as.magpie(ordered_data, spatial=1, temporal=2)
-    return(x)
+  ordered_data <- expanded_data[, c("region", "time", "end_use", "value")]
+
+  x <- magclass::as.magpie(ordered_data, spatial = 1, temporal = 2)
+  return(x)
 }
