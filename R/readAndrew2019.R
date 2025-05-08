6e23df3b90d2d20e4c600aea4af4898ff32fed3e
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-05-09 00:13:23 +0200

    update to newest version of Andrew dataset

--- R/readAndrew2019.R
+++ R/readAndrew2019.R
@@ -1,17 +1,18 @@
 #' Read Data from Andrew's 2019 paper
 #'
-#' https://zenodo.org/records/11207133
-#' Last version update 17.05.2024
+#' https://zenodo.org/records/14931651
+#' Last version update 26.02.2025
 #' Dataset update from:
 #' Andrew, R.M., 2019. Global CO2 emissions from cement production, 1928–2018.
 #' Earth System Science Data 11, 1675–1710. https://doi.org/10.5194/essd-11-1675-2019.
 #' @author Bennet Weiss
 #' @param subtype Material subtype. Can be "cement" or "clinker".
 readAndrew2019 <- function(subtype) {
+  folder <- "v2"
   if(subtype == "cement") {
-    path <- file.path("v1", "1. annual_cement_production.csv")
+    path <- file.path(folder, "1. annual_cement_production.csv")
   } else if (subtype == "clinker") {
-    path <- file.path("v1", "2. annual_clinker_production.csv")
+    path <- file.path(folder, "2. annual_clinker_production.csv")
   } else {
     stop("Invalid subtype. Choose either 'cement' or 'clinker'.")
   }
