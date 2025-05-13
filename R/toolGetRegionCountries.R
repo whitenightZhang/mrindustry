b06e59b5dab6b96223c92b9cd21e2a73bb57ea6f
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-05-13 09:28:26 +0200

    refactor: replace getRegionCountries calls with toolGetRegionCountries and move function to new file

---
+++ R/toolGetRegionCountries.R
@@ -0,0 +1,18 @@
+#' @author Bennet Weiss
+#' @param x Magpie object
+#' @param region string or vector of strings to identify the region
+#' @param regionmapping mapping of regions to countries
+toolGetRegionMask <- function(x, region, regionmapping) {
+  mask <- magclass::getItems(x, dim = 1) %in% regionmapping$CountryCode[regionmapping$RegionCode %in% region]
+  return(mask)
+}
+
+#' @author Bennet Weiss
+#' @param x Magpie object
+#' @param region string or vector of strings to identify the region
+#' @param regionmapping mapping of regions to countries
+toolGetRegionCountries <- function(x, region, regionmapping) {
+  regionmask <- toolGetRegionMask(x, region, regionmapping)
+  countries <- magclass::getItems(x, dim = 1)[regionmask]
+  return(countries)
+}
