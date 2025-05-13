f7e340f15fca838fd6a2cc2622fba983e9156011
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-05-13 09:29:09 +0200

    refactor: rename calcCementProduction function to calcMineralProduction for clarity

--- R/calcMineralProduction.R
+++ R/calcMineralProduction.R
@@ -1,7 +1,7 @@
 #' Calculates global cement production as from Andrew's 2019 paper.
 #' @author Bennet Weiss
 #' @param subtype Material subtype. Can be "cement or "clinker".
-calcCementProduction <- function(subtype) {
+calcMineralProduction <- function(subtype) {
   x <- readSource("Andrew2019", subtype)
   x[is.na(x)] <- 0
   x <- x * 1e3 # convert to tonnes
