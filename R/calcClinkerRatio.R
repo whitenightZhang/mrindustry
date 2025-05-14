2b668762700d0e483b996c38629faaea85b184b4
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-05-14 17:46:15 +0200

    refactor: move NA handling functions to a new file for better organization

--- R/calcClinkerRatio.R
+++ R/calcClinkerRatio.R
@@ -61,25 +61,3 @@ calcClinkerRatio <- function() {
   output <- list(x = ratio, weight = weight, unit = unit, description = description)
   return(output)
 }
-
-#' Removes NA values from a Magpie object. Only works if all regions show the same temporal pattern of data scarcity.
-#' @author Bennet Weiss
-#' @param x Magpie object
-toolRemoveNA <- function(x) {
-  df <- as.data.frame(x, rev=3)
-  df_clean <- df[complete.cases(df), ]
-  df_clean <- na.omit(df)
-  x_clean <- as.magpie(df_clean)
-  return(x_clean)
-}
-
-#' Create mask for countries that have >= n_noNA NA values.
-#' @author Bennet Weiss
-#' @param x Magpie object
-#' @param n_noNA Minimum values not NA values of a country to be included in the mask
-toolMaskNACountries <- function(x, n_noNA = 5) {
-  arr <- as.array(x)
-  counts <- apply(!is.na(arr), 1, sum)
-  mask <- names(counts[counts < n_noNA])
-  return(mask)
-}
