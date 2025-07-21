97e0ade08a7c108c724386edcc4ea630e31a6f83
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2025-07-21 20:48:56 +0200

    Remove duplicate code withCCS/noCCS

--- R/calcAllChemicalRoute2020.R
+++ R/calcAllChemicalRoute2020.R
@@ -2,26 +2,28 @@
 #' @author Qianzhi Zhang
 #'
 #' @export
-calcAllChemicalRoute2020withCCS <- function() {
+#' @param CCS boolean parameter whether CCS technologies are considered as such in 2020 or assumed to be technologies without CCS
+#' 
+calcAllChemicalRoute2020withCCS <- function(CCS=FALSE) {
   
   AllChemicalRoute2020<- calcOutput("AllChemicalRoute2005_2020",warnNA = FALSE, aggregate = TRUE)[,"y2020",]%>% 
     as.data.frame() %>%
     select(-Cell)
   
-  Chemcial_Total <- calcOutput("ChemicalTotal", aggregate = FALSE)%>%
+  Chemical_Total <- calcOutput("ChemicalTotal", aggregate = FALSE)%>%
     .[,c("y2020"),]
   
   map <- toolGetMapping("regionmappingH12.csv", type = "regional", where = "mrindustry") 
   
   x <- as.magpie(AllChemicalRoute2020, spatial = 1, temporal = 2)
   # Aggregate the regional data (xReg) to the country level using the mapping
-  x <- toolAggregate(x, rel = map, dim = 1, from = "RegionCode", to = "CountryCode", weight = Chemcial_Total[unique(map$CountryCode), , ])
+  x <- toolAggregate(x, rel = map, dim = 1, from = "RegionCode", to = "CountryCode", weight = Chemical_Total[unique(map$CountryCode), , ])
   x[is.na(x)] <- 0
   
   return(list(
     x = x,
     weight = NULL,
-    unit = "TODO ???",
-    description = "TODO ???"
+    unit = "Gt Chemical Routes",
+    description = "Chemical route flows filtered for the year 2020"
   ))
 }
